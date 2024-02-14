34a4b6dfac1262cd5e3c5f8b7988575aee1c47e1
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-02-14 13:17:38 -0800

    Add event listener to search input
    Co-authored-by: hiem55 <hiem55@users.noreply.github.com>

---
+++ client/scripts/search.js
@@ -0,0 +1,11 @@
+class Search {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("search").addEventListener("input", () => {
+        console.log("search updated");
+    });
+});
+
+export default Search;
