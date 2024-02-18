45ce03634e54398d28696c448035bbee5133c8f9
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-02-18 15:38:23 -0800

    Create search methods

--- client/scripts/search.js
+++ client/scripts/search.js
@@ -1,10 +1,28 @@
 class Search {
+    getResults(query) {
+        // TODO: Change localhost to the server's address
+        xhr.open("GET", "http://localhost:8000", true);
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                return this.displayResults(xhr.responseText);
+            }
+        };
+    }
 
+    displayResults(results) {
+        document.getElementById("response-word").innerHTML = results.word;
+        document.getElementById("response-definition").innerHTML = results.definition;
+    }
 }
+
+const xhr = new XMLHttpRequest();
+const search = new Search();
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("search").addEventListener("input", () => {
-        console.log("search updated");
+        // TODO: Input validation
+        search.getResults(document.getElementById("search").value);
     });
 });
 
