f6e332ff12c7b5a9796c5cdc85bcf279398211d8
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-02-18 18:06:12 -0800

    Create separate server-side user js file

---
+++ server/user.js
@@ -0,0 +1,7 @@
+const message = {
+    dictionaryEmptyError: "Error! Dictionary is currently empty.",
+    searchError: "Error! Word not found.",
+    warning: "Warning! %s already exists."
+}
+
+module.exports = message;
