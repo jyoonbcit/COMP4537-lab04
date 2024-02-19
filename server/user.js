e06cde04e44acddc4e8247b1dec2cf6ab3137f99
hiem55 <100002888+hiem55@users.noreply.github.com>
2024-02-18 18:41:05 -0800

    update user facing text

--- server/user.js
+++ server/user.js
@@ -1,7 +1,9 @@
 const message = {
     dictionaryEmptyError: "Error! Dictionary is currently empty.",
     searchError: "Error! Word not found.",
-    warning: "Warning! %s already exists."
+    warning: "Warning! %s already exists.",
+    count: "Request # %s",
+    success: 'New entry recorded /n "%s: %t"'
 }
 
 module.exports = message;
